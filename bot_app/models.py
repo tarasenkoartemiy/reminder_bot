4809f044a52f96442401ac48c1fb2e7da1a8898e
Artem Tarasenko <tarasenko.artyem@gmail.com>
2023-03-09 09:27:15 +0300

    Feat: Create a table in the database for notes
    A separate table has been created for storing notes for better structuring of data.

--- bot_app/models.py
+++ bot_app/models.py
@@ -9,18 +9,29 @@ class User(models.Model):
     language = models.CharField(max_length=2, default=None, null=True)
     time_zone = models.CharField(max_length=50, default=None, null=True)
     status = models.CharField(max_length=15)
+    change_number = models.IntegerField(default=None, null=True)
     reminder_count = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.username}:{self.id}"
+        return f"| USERNAME:{self.username} | USER_ID:{self.id} |"
 
 
 class Reminder(models.Model):
-    id = models.IntegerField(primary_key=True)
+    reminder_id = models.IntegerField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
+    reminder_text = models.TextField()
     date_time = models.DateTimeField(default=None, null=True)
     is_active = models.BooleanField(default=None, null=True)
 
     def __str__(self):
-        return f"{self.text.split()[0]}:{self.id}:{self.user_id}"
+        return f"| REMINDER:{self.reminder_text} | REMINDER_ID:{self.reminder_id} | USER_ID:{self.user_id} |"
+
+
+class Note(models.Model):
+    note_id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    note_text = models.TextField()
+    possible_date = models.DateTimeField(default=None, null=True)
+
+    def __str__(self):
+        return f"| NOTE:{self.note_text} | NOTE_ID:{self.note_id} | USER_ID:{self.user_id} |"
