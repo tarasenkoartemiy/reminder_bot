ed617f552b20ca464090db6f98b4e2d043b66cbb
Artem Tarasenko <tarasenko.artyem@gmail.com>
2023-03-09 13:42:33 +0300

    Fix: Implement proper handling of api exceptions

--- bot_app/timezone.py
+++ bot_app/timezone.py
@@ -1,22 +1,24 @@
+from requests import ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError, get
 from datetime import datetime
-import requests
 
 
 def getting_coordinates(city: str):
     nominatim_api_link = "https://nominatim.openstreetmap.org/search?"
     try:
-        if not (response := requests.get(nominatim_api_link, params={"q": city, "limit": 1, "format": "json"}).json()):
+        if not (response := get(nominatim_api_link, params={"q": city, "limit": 1, "format": "json"}).json()):
             return False
-    except requests.exceptions:
+    except (ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError) as coordinates_exception:
+        print(coordinates_exception)
         return None
 
     def getting_timezone():
         time_api_link = "https://www.timeapi.io/api/Time/current/coordinate?"
-        lat = response[0]["lat"]
-        lon = response[0]["lon"]
+        lat = response[0].get("lat")
+        lon = response[0].get("lon")
         try:
-            return requests.get(time_api_link, params={"latitude": lat, "longitude": lon}).json()["timeZone"]
-        except requests.exceptions:
+            return get(time_api_link, params={"latitude": lat, "longitude": lon}).json()["timeZone"]
+        except (ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError) as timezone_exception:
+            print(timezone_exception)
             return None
         except KeyError:
             return False
