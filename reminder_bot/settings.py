063dce26d21a04539d1a8f706d09c97c3aa45dd6
Artem Tarasenko <tarasenko.artyem@gmail.com>
2023-03-10 19:42:08 +0300

    Feat: Switch to postgresql
    The application being developed is multi-user, it is better to choose postgresql

--- reminder_bot/settings.py
+++ reminder_bot/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'reminder_bot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST')
     }
 }
 
